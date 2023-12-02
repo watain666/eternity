d1cb8d68db330580a68e9c487930c4eba2f64e6a
watain666 <watain666666@gmail.com>
2023-12-03 02:17:05 +0800

    add: jquery goup button and goBottom func for down-arrow

---
+++ static/js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // go up button
+    $.goup({
+        trigger: 500,
+        bottomOffset: 10,
+        locationOffset: 20,
+        containerRadius: 0,
+        containerColor: '#fff',
+        arrowColor: '#000',
+        goupSpeed: 'normal'
+    });
+});
+
+function goBottom() {
+    console.log("goBottom clicked")
+    console.log($(document).height())
+    $('html, body, .slider').animate({scrollTop: $(document).height() }, 750);
+}
